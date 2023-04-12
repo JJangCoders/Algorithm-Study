b5237c3b70a7ff482924abc7b68629e715dcf5f3
Sohn Young Jin <sonkim1001@naver.com>
2023-04-12 17:45:05 +0900

    [BOJ] 1316 그룹 단어 체커

---
+++ ILoveGameCoding/20230413/1316.py
@@ -0,0 +1,21 @@
+def check_group_word(word):
+    group = {}
+    before_ch = '0'
+
+    for ch in word:
+        if before_ch != ch:
+            if ch in group:
+                return False
+
+            group[ch] = 1
+            before_ch = ch
+
+    return True
+
+
+result = 0
+
+for i in range(0, int(input())):
+    result += 1 if check_group_word(input()) else 0
+
+print(result)
