aed9c09744a9e88855f94aaf5603a973e5873248
junho <junho1185@gmail.com>
2023-04-13 21:28:15 +0900

    [BOJ] 10773 제로

--- Junho Park/20230413/10773.py
+++ Junho Park/20230413/10773.py
@@ -2,19 +2,19 @@ from sys import stdin
 
 n = int(stdin.readline())
 
-numbers = []
+numbers = []    #array to store input numbers
 
 for i in range(n):
-    number = int(stdin.readline())
+    number = int(stdin.readline())  #input number
 
     if number == 0:
-        del numbers[len(numbers) - 1]
+        del numbers[len(numbers) - 1]   #removing the last element of the array
     else:
-        numbers.append(number)
+        numbers.append(number)      #adding the number in the end
 
 sum = 0
 
-for number in numbers:
+for number in numbers:      #loop to sum up the numbers
     sum += number
 
-print(sum)
+print(sum)     #output
