568f1fad465572c4611ca3c2d7ba772949b58e62
junho <junho1185@gmail.com>
2023-04-15 23:01:51 +0900

    [BOJ] 1253 좋다!

--- Junho Park/20230420/1253.py
+++ Junho Park/20230420/1253.py
@@ -29,3 +29,56 @@ for i in range(N):
         else:
             left += 1
 print(good_count)
+
+#여기서부턴 제가 원래 이진탐색 방법으로 풀려했던 코드입니다.
+# from sys import stdin
+
+# N = int(stdin.readline())
+# numbers = list(map(int, stdin.readline().split()))
+# sorted_numbers = sorted(numbers)
+# #input and sorting
+
+# smallest_number = sorted_numbers[0]
+# second_smallest_number = sorted_numbers[1]
+# good_count = 0
+# good_number_found_flag = False
+# #init values
+
+# for number in sorted_numbers:
+#     if number < smallest_number + second_smallest_number:
+#         continue
+    
+#     good_number_found_flag = False
+#     upper_cap = N//2
+
+#     if number <= sorted_numbers[upper_cap]:
+#         while number <= sorted_numbers[upper_cap]:
+#             upper_cap = upper_cap//2
+#         if number > sorted_numbers[upper_cap]:
+#             upper_cap *= 2
+#     else:
+#         while number > sorted_numbers[upper_cap]:
+#             upper_cap = upper_cap + (N - upper_cap)//2
+#     #taking care of the upper bound
+
+#     lower_cap = 0
+#     exp = 0
+
+#     while sorted_numbers[lower_cap] + sorted_numbers[upper_cap] < number:
+#         lower_cap = 2**exp
+#         exp += 1
+#     lower_cap = lower_cap // 2
+    
+#     for i in range(upper_cap + 1):
+#         for j in range(i + 1, upper_cap + 1):
+#             result = sorted_numbers[i] + sorted_numbers[j]
+#             if result == number:
+#                 good_count += 1
+#                 good_number_found_flag = True
+#                 break
+#             elif result > number:
+#                 break
+#         if good_number_found_flag:
+#             break
+
+# print(good_count)
