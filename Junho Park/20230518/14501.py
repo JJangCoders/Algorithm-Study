c5541b52f86e66901d14d87aef35b58e008f99df
junho <junho1185@gmail.com>
2023-05-16 20:58:36 +0900

    [BOJ] 14501 퇴사

--- Junho Park/20230518/14501.py
+++ Junho Park/20230518/14501.py
@@ -1,6 +1,8 @@
 # This is my solution for BaekJoon 14501
 # 
 # https://www.acmicpc.net/problem/14501
+#
+# Reference: https://great-park.tistory.com/48
 
 from sys import stdin
 
@@ -15,10 +17,14 @@ for i in range(N):
     length, profit = map(int, stdin.readline().split())
 
     # can't do it, skip
-    if i + length >= N:
-        continue
+    consults.append((length, profit))
 
-    consults.append((i+1, length, profit))
+# array to store maximum cost
+maxCost = [0 for _ in range(N+1)]
 
-# number of possible consults
-L = len(consults)
+for i in range(N):
+    for j in range(i+consults[i][0], N+1):
+        if maxCost[j] < maxCost[i] + consults[i][1]:
+            maxCost[j] = maxCost[i] + consults[i][1]
+
+print(maxCost[-1])
